e8e2cd4ed86b6c36ed5672ac034b361529e87dd5
Bazil M <bazilm33@gmail.com>
2015-09-20 03:01:31 +0530

    Added wrapper for user info, subreddit info,voting.

--- js/redditWrapper.js
+++ js/redditWrapper.js
@@ -1,15 +1,35 @@
 var token;
 var xmlhttp;
-
+var modhash;
 function init(tok)
 {
   token = tok;
   xmlhttp = new XMLHttpRequest();
+  get_mod_hash();
 }
 
+
+function get_mod_hash()
+{
+  xmlhttp.onreadystatechange=function()
+  {
+  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+    console.log(xmlhttp.responseText);
+    //set modhash here
+    }
+  }
+
+  xmlhttp.open("GET","https://oauth.reddit.com/api/v1/me",true);
+  xmlhttp.setRequestHeader("Authorization","bearer " + token);
+  xmlhttp.send();
+
+
+
+}
 function get_user_info()
 {
-  
+
   xmlhttp.onreadystatechange=function()
   {
   if (xmlhttp.readyState==4 && xmlhttp.status==200)
@@ -37,5 +57,18 @@ function get_subreddits()
   xmlhttp.open("GET","https://oauth.reddit.com/subreddits/mine",true);
   xmlhttp.setRequestHeader("Authorization","bearer " + token);
   xmlhttp.send();
+
+}
+
+function vote(id,dir)
+{
+
+
+  xmlhttp.open("POST","https://oauth.reddit.com/api/vote",true);
+  xmlhttp.setRequestHeader("dir",dir);
+  xmlhttp.setRequestHeader("id",id);
+  xmlhttp.setRequestHeader("X-Modhash",modhash);
+  xmlhttp.send();
+
 
 }
